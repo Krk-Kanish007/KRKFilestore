de2aaa847849c16f8710cd6b64f4ae922f4fac9e
ThalaThalapathy <106385818+ThalaThalapathy@users.noreply.github.com>
2022-08-07 16:27:16 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -95,7 +95,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
                  [InlineKeyboardButton("Bots Channel", url="https://t.me/+SUmcuvpkK6E2M2Zl"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/+ekHFpHjqdn4zMDc1")]
+                  InlineKeyboardButton("Support Group", url="https://t.me/+ekHFpHjqdn4zMDc1")]]
             ),
             disable_web_page_preview=True
         )
